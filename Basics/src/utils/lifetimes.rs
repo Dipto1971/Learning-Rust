7a050750a7b6ed801aa49a874268d36d446c04d7
Mahir Faysal Dipto <86009555+Dipto1971@users.noreply.github.com>
2025-03-20 11:23:55 +0600

    Refactor longest_with_reference function to longest_using_generic_lifetime for improved lifetime handling

--- Basics/src/utils/lifetimes.rs
+++ Basics/src/utils/lifetimes.rs
@@ -8,21 +8,29 @@ fn longest(a: String, b:String) -> String {
     }
 }
 
-fn longest_with_reference(a: &str, b:&str) -> &str{
-    if a.len() > b.len() {
-        return a;
-    } else {
-        return b;
-    }
+// fn longest_with_reference(a: &str, b:&str) -> &str{
+//     if a.len() > b.len() {
+//         return a;
+//     } else {
+//         return b;
+//     }
 //     Why Does This Fail?
 // The function returns either a or b, both of which are borrowed references.
 // However, Rust needs to know how long the returned reference will live.
 // Since a and b have different lifetimes (potentially coming from different scopes), 
 // the compiler cannot determine a single lifetime for the return value.
-}
+// }
 
 fn longest_lifetime() {
     
+}
+
+fn longest_using_generic_lifetime <'a>(first: &'a str, second: &'a str) -> &'a str { //Lifespan of all the variables will be 'a
+    if first.len() > second.len() {
+        return first;
+    }else {
+        return second;
+    }
 }
 
 // fn main() {
@@ -39,14 +47,23 @@ fn longest_lifetime() {
 
 // But what if we pass the reference
 
-fn main() {
-    let ans;
-    let str1 = String::from("Small");
-    {
-        let str2 = String::from("Longer");
+// fn main() {
+//     let ans;
+//     let str1 = String::from("Small");
+//     {
+//         let str2 = String::from("Longer");
 
-        ans = longest_with_reference(&str1, &str2);
-        // str2 is created inside an inner scope { ... }, but ans is assigned outside that scope.
-        // When str2 goes out of scope, it is dropped, but ans still holds a reference to it, leading to a dangling reference.
-    }
+//         ans = longest_with_reference(&str1, &str2);
+//         // str2 is created inside an inner scope { ... }, but ans is assigned outside that scope.
+//         // When str2 goes out of scope, it is dropped, but ans still holds a reference to it, leading to a dangling reference.
+//     }
+// }
+
+fn main() {
+    let longest;
+    let str1= String::from("Small passed in a function using lifetimes");
+    let str2= String::from("Longer passsed in a function using lifetimes");
+
+    longest = longest_using_generic_lifetime(&str1, &str2);
+    println!("{}", longest);
 }
